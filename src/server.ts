004bcdae44be8367902e98044ad988ab4e0f1e7e
webmaster1022 <seniordev1019@gmail.com>
2021-10-16 19:19:08 +0200

    feat:Add orders models and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import products_routes from './controllers/products'
+import orders_routes from './controllers/orders'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -12,6 +13,7 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 products_routes(app);
+orders_routes(app);
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
 })
