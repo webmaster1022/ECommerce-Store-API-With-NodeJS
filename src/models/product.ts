1feeb35cb01c7a99df9c06c865601d9811eadbf1
webmaster1022 <seniordev1019@gmail.com>
2021-10-16 17:37:49 +0200

    Feat:Add CRUD model for product

---
+++ src/models/product.ts
@@ -0,0 +1,60 @@
+import client from "../database";
+
+
+export type Product = {
+    id:number;
+    name:string;
+    price:number;
+    cathegory:string
+}
+
+export class productStore{
+    async index(): Promise<Product[]> {
+       try {
+        const conn = await client.connect(); //connectos to database
+        const sql = 'SELECT * FROM products';//SQL command to retrieve data from database table
+        const result = await conn.query(sql); //store results from sql query
+        conn.release(); //close database connection
+        return result.rows;
+       } catch (error) {
+           throw new Error (`Cannot retrieve prodructs ${error}`)
+       }
+    }
+
+    async show(id:string):Promise<Product> {
+        try {
+            const conn = await client.connect(); //connectos to database
+            const sql = `SELECT * FROM products WHERE id=($1)`;//SQL command to retrieve data from database table
+            const result = await conn.query(sql,[id]); //store results from sql query
+            conn.release(); //close database connection
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Cannot retrieve product with id:${id} ${error}`)
+        }
+    }
+
+    async create(p:Product):Promise<Product> {
+        try {
+            const conn = await client.connect(); //connectos to database
+            const sql = `INSERT INTO products (name,price,cathegory) VALUES($1,$2,$3) RETURNING *`;//SQL command to retrieve data from database table
+            const result = await conn.query(sql,[p.name,p.price,p.cathegory]); //store results from sql query
+            conn.release(); //close database connection
+            const product = result.rows[0];
+            return product;
+        } catch (error) {
+            throw new Error(`Cannot create product ${p} ${error}`)
+        }
+    }
+
+    async delete(id:string):Promise<Product> {
+        try {
+            const conn = await client.connect(); //connectos to database
+            const sql = `DELETE * FROM products WHERE id=($1)`;//SQL command to retrieve data from database table
+            const result = await conn.query(sql,[id]); //store results from sql query
+            conn.release(); //close database connection
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Cannot retrieve product with id:${id} ${error}`)
+        }
+    }
+}
