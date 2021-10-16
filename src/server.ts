955901ed319228bc1faf62ff70dd05e597395195
webmaster1022 <seniordev1019@gmail.com>
2021-10-16 19:01:54 +0200

    Feat: Add product routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import products_routes from './controllers/products'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
+
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
-
+products_routes(app);
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
 })
