48308c5f4d91bf687d9759c870cfed8952384f76
webmaster1022 <seniordev1019@gmail.com>
2021-10-17 16:23:41 +0200

    Add middleware for jwt verification

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,4 +1,4 @@
-import express,{Request,Response} from "express";
+import express,{Request,Response,NextFunction} from "express";
 import { Product,productStore } from "../models/product";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -6,6 +6,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const store = new productStore();
+
+const verifyAuthToken = (req: Request, res: Response, next:NextFunction) => {
+    try {
+        const authorizationHeader = (req.headers.authorization) as string
+        const token = authorizationHeader.split(' ')[1]
+        const decoded = jwt.verify(token, (process.env.TOKEN_SECRET) as string)
+
+        next()
+    } catch (error) {
+        res.status(401)
+        res.json(`Access denied,Invalid token. Error: ${error}`)
+    }
+}
 
 const index = async (_req:Request,res:Response)=>{
     const products = await store.index();
@@ -18,17 +31,15 @@ const show = async(req:Request,res:Response)=>{
 }
 
 const create = async (req: Request, res: Response) => {
-    
+    try {
         const product: Product = {
             name: req.body.name,
             price: req.body.price,
             cathegory:req.body.cathegory
 
         }
-        try {
-            const authorizeHeader = (req.headers.authorization) as string;
-            const token = authorizeHeader.split(' ')[1];
-            jwt.verify(token,(process.env.TOKEN_SECRET) as string)
+        
+            
         const newProduct = await store.create(product)
         res.json(newProduct)
     } catch(err) {
@@ -45,7 +56,7 @@ const remove = async (req: Request, res: Response) => {
 
 const products_routes=(app: express.Application) =>{
     app.get('/products',index)
-    app.post('/products',create)
+    app.post('/products',verifyAuthToken,create)
     app.get('/products/:id',show)
     app.delete('/products/:id',remove)
 }
