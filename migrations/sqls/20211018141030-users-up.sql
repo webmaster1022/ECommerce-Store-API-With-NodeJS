661e99d826c08db9758081a2dc410bb10b5797ec
webmaster1022 <seniordev1019@gmail.com>
2021-10-18 20:21:55 +0200

    fix:modify types in the database migration

--- migrations/sqls/20211018141030-users-up.sql
+++ migrations/sqls/20211018141030-users-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users(id SERIAL PRIMARY KEY,first_name VARCHAR(255),last_name VARCHAR(255),username VARCHAR(100),password_digest VARCHAR(255));
+CREATE TABLE users(id SERIAL PRIMARY KEY,first_name VARCHAR(255),last_name VARCHAR(255),username VARCHAR(100),password VARCHAR(255));
--- migrations/sqls/20211018141355-orders-up.sql
+++ migrations/sqls/20211018141355-orders-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE orders(id SERIAL PRIMARY KEY,status VARCHAR(64),user_id bigint REFERENCES users(id));
+CREATE TABLE orders(id SERIAL PRIMARY KEY,status VARCHAR(64),user_id integer REFERENCES users(id));
